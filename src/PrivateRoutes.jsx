cba2c832f7f70e84bf9de72b9c27f927f8f4648a
HamzaAliKUndi <hamza.alee83@gmail.com>
2023-07-17 16:11:12 +0500

    Set PrivateRoutes Com

--- src/PrivateRoutes.jsx
+++ src/PrivateRoutes.jsx
@@ -3,12 +3,8 @@ import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoutes = () => {
   const token = localStorage.getItem("token");
-  if (token) {
-    return <Outlet />;
-  } else {
-    return <Navigate to="/dashboard" />;
-  }
-  //   return <>{token ? <Outlet /> : <Navigate to="/" />}</>;
+
+  return <>{token ? <Outlet /> : <Navigate to="/" />}</>;
 };
 
 export default PrivateRoutes;
